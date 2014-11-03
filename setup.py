97c4d27ac80835ceabb352af10e4bc07d2119cd8
Chang-Hung Liang <cliang@machinezone.com>
2014-11-03 13:57:57 -0800

    Use pylibmc dev version to support Python 3

--- setup.py
+++ setup.py
@@ -46,14 +46,20 @@ if py3:
     memcached_requires = [
         'python3-memcached>=1.51',
     ]
+
+    # XXX: pylibmc hasn't officially supported Python 3, use development
+    # version
+    pylibmc_requires = [
+        'https://github.com/lericson/pylibmc/archive/master.zip',
+    ]
 else:
     memcached_requires = [
         'python-memcached>=1.53',
     ]
 
-pylibmc_requires = [
-    'pylibmc>=1.3.0,<1.4.0',
-]
+    pylibmc_requires = [
+        'pylibmc>=1.3.0,<1.4.0',
+    ]
 
 redis_requires = [
     'hiredis>=0.1.5,<0.2.0',
